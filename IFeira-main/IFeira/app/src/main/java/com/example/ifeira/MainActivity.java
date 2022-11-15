c5d83451627dc53435eb2ed76a82d9cdeaa2e25f
grupogjm <116836717+grupogjm@users.noreply.github.com>
2022-11-14 23:59:28 -0400

    Adicionar Layouts e Recursos
    Layout de Login, Layout de Recuperar Senha, Layout de Detalhes

---
+++ IFeira-main/IFeira/app/src/main/java/com/example/ifeira/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ifeira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*View start = findViewById(R.id.rl);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, TelaDePagamento.class);
+                startActivity(i);
+            }
+        });*/
+    }
+}
