e20bdbef0d19df781bd27d8acc68d3c5bfaf85f6
Moises-Paz <moisespaz678@gmail.com>
2022-11-19 20:51:05 -0400

    agora sim o commmit antes de baixar e mesclar como github

--- IFeira/app/src/main/java/com/example/ifeira/MainActivity.java
+++ IFeira/app/src/main/java/com/example/ifeira/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ifeira;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,12 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*View start = findViewById(R.id.rl);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        View start = findViewById(R.id.View_ic_tres_linhas);
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, TelaDePagamento.class);
+                Intent i = new Intent(MainActivity.this, MeusProdutos.class);
                 startActivity(i);
             }
-        });*/
+        });
     }
 }
---
+++ IFeira/app/src/main/java/com/example/ifeira/TelaDeConversas.java
@@ -0,0 +1,18 @@
+package com.example.ifeira;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TelaDeConversas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_conversas);
+
+    }
+}
